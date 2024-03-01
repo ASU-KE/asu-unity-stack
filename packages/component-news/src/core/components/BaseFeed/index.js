6e89f2f02b8e1cd1aa3bee8ac2003ca828d804fd
John Cox <john.k.cox@asu.edu>
2024-03-01 10:22:34 -0700

    feat(core/components): custom feed container providers: ASU news, KE news, merged feed

--- packages/component-news/src/core/components/BaseFeed/index.js
+++ packages/component-news/src/core/components/BaseFeed/index.js
@@ -3,18 +3,22 @@ import {
   FeedBody,
   FeedContainerProvider,
   FeedHeader,
-} from "@asu/components-core";
+  DrupalFeedContainerProvider,
+  KeNewsContainerProvider,
+  MergedNewsContainerProvider,
+} from "../../../../../components-core/src";
 import PropTypes from "prop-types";
 import React from "react";
 
-import {
-  feedHeaderShape,
-  feedCtaButtonShape,
-  feedDataSourceShape,
-} from "../../../../../components-core/src/components/FeedAnatomy/feed-prop-types";
+// import {
+//   feedHeaderShape,
+//   feedCtaButtonShape,
+//   feedDataSourceShape,
+// } from "../../../../../components-core/src/components/FeedAnatomy/feed-prop-types";
 import { defaultProps } from "../../constants/default-props";
-import { filterData } from "../../services/data-manager";
-import { transformData } from "../../transformers/transform-data";
+import { filterDrupalData } from "../../services/data-manager";
+import { transformData as transformDrupalData } from "../../transformers/drupal.transformer";
+import { transformData as transformWpData } from "../../transformers/wordpress.transformer";
 
 /**
  * @typedef {import("@asu/components-core/src/core/types/feed-types").FeedType} FeedType
@@ -27,19 +31,20 @@ const BaseFeed = ({
   children,
   header,
   ctaButton,
-  dataSource: pDataSource,
+  drupalDataSource,
+  wpDataSource,
   maxItems,
 }) => {
-  const filters = pDataSource.filters?.replace(/_/g, " ");
-  const dataSource = { ...pDataSource, filters };
-
+  const filters = drupalDataSource?.filters?.replace(/_/g, " ");
+  const formattedDrupalDataSource = drupalDataSource ? { ...drupalDataSource, filters } : null;
+  if (drupalDataSource && !wpDataSource) {
   // We provide in the renderBody the view specified before in the parent component, recieved as "children" in this component.
   // We provide in the renderHeader the components-core header, if it is desired to be shown
   // We provide the maxItems prop to limit the items rendered
   // We provide the dataSource to read the url to fetch the data
   // We provide the defaultProps to use some needed default values in case they are not provided
   return (
-    <FeedContainerProvider
+    <DrupalFeedContainerProvider
       renderHeader={
         header && ctaButton ? (
           <FeedHeader
@@ -50,16 +55,62 @@ const BaseFeed = ({
         ) : null
       }
       renderBody={<FeedBody>{children}</FeedBody>}
-      dataTransformer={transformData}
-      dataFilter={filterData}
-      dataSource={dataSource}
+      dataTransformer={transformDrupalData}
+      dataFilter={filterDrupalData}
+      dataSource={formattedDrupalDataSource}
       defaultProps={defaultProps}
       noFeedText="No news to show."
       maxItems={maxItems}
     />
   );
+  }
+  if (!drupalDataSource && wpDataSource) {
+    return (
+      <KeNewsContainerProvider
+        renderHeader={
+          header && ctaButton ? (
+            <FeedHeader
+              header={header}
+              ctaButton={ctaButton}
+              defaultProps={defaultProps}
+            />
+          ) : null
+        }
+        renderBody={<FeedBody>{children}</FeedBody>}
+        wpDataSource={wpDataSource}
+        wpDataTransformer={transformWpData}
+        noResultsText="No news to show."
+        maxItems={maxItems}
+      />
+    );
+  }
+  if (drupalDataSource && wpDataSource) {
+    return (
+      <MergedNewsContainerProvider
+        renderHeader={
+          header && ctaButton ? (
+            <FeedHeader
+              header={header}
+              ctaButton={ctaButton}
+              defaultProps={defaultProps}
+            />
+          ) : null
+        }
+        renderBody={<FeedBody>{children}</FeedBody>}
+        defaultProps={defaultProps}
+        drupalDataSource={formattedDrupalDataSource}
+        drupalDataFilter={filterDrupalData}
+        drupalDataTransformer={transformDrupalData}
+        wpDataSource={wpDataSource}
+        wpDataTransformer={transformWpData}
+        noResultsText="No news to show."
+        maxItems={maxItems}
+      />
+    );
+  }
+  return null;
 };
-
+/*
 BaseFeed.propTypes = {
   header: feedHeaderShape,
   ctaButton: feedCtaButtonShape,
@@ -67,5 +118,5 @@ BaseFeed.propTypes = {
   maxItems: PropTypes.number,
   children: PropTypes.element,
 };
-
+*/
 export { BaseFeed };
