c5ce4ce865057e9e469a0afaf2ca612ce5a0b4a5
Scott Williams <5209283+scott-williams-az@users.noreply.github.com>
2025-01-14 12:54:48 -0700

    Update manager.jsx

--- packages/unity-react-core/.storybook/manager.jsx
+++ packages/unity-react-core/.storybook/manager.jsx
@@ -4,7 +4,6 @@ import { addons } from '@storybook/manager-api';
 addons.setConfig({
   sidebar: {
     renderLabel: (item) =>{
-    // console.log(item)
       return <div style={{fontSize: "1rem"}}>{item.name}</div>
     }
   },
