f157a972f6b4d4bed9bcfb1394442e2dd00c6be6
Scott Williams <stwilli2@asu.edu>
2025-02-12 07:36:23 -0700

    chore: better doc anchor menu

--- packages/unity-bootstrap-theme/stories/atoms/anchor-menu/anchor-menu.examples.stories.js
+++ packages/unity-bootstrap-theme/stories/atoms/anchor-menu/anchor-menu.examples.stories.js
@@ -1,14 +1,33 @@
 import React from "react";
 
+import { defaultDecorator } from "../../../../../shared/components/Layout";
+
 export default {
   title: "Atoms/Anchor Menu",
+    decorators: [ defaultDecorator ],
+    parameters: {
+    controls: { disable: true },
+    docs: {
+      description: {
+        component: `This example has moved to unity-react-core.
+
+React components and HTML templates are now located in a shared space to ensure consistency between the React and Bootstrap usages.
+        `,
+      },
+    },
+  }
 };
 
 
-export const AnchorMenu = () => {
-return (
-    <>
-      <a target="parent" href="../../@asu/unity-react-core/index.html?path=/story/components-anchormenu--default">See @asu/unity-react-core</a>
-    </>
-  )
+export const AnchorMenu = {
+  render: () => <p>
+    Follow the link to view the{" "}
+    <a
+      href="../../@asu/unity-react-core/index.html?path=/story/components-anchormenu--default"
+      target="_parent"
+    >
+      anchor menu
+    </a>{" "}
+    example in @asu/unity-react-core.
+  </p>
 };
