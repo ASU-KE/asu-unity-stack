853280d9b3b74484f7f70fb53833eaf2c3024c09
Scott Williams <stwilli2@asu.edu>
2025-02-13 10:28:19 -0700

    unused vars and formatting

--- packages/static-site/src/components/Card.tsx
+++ packages/static-site/src/components/Card.tsx
@@ -1,4 +1,3 @@
-import { Card, ButtonProps } from "@asu/unity-react-core";
 import { FC } from "react";
 import { getRelativePath } from "~/utils/baseUrl";
 
@@ -6,31 +5,33 @@ export interface CardWrapperProps {
   title: string;
   description: string | JSX.Element;
   href: string;
-  linkLabel: string
+  linkLabel: string;
   isRoute?: boolean;
 }
-const CardWrapper:FC<CardWrapperProps> = ({title, description, href,linkLabel, isRoute}) => {
+const CardWrapper: FC<CardWrapperProps> = ({
+  title,
+  description,
+  href,
+  linkLabel,
+  isRoute,
+}) => {
   return (
     <div className="col-md-6 pb-8">
       <h3>{title}</h3>
-      <p>
-        {description}
-      </p>
+      <p>{description}</p>
       <p>
         {isRoute ? (
-          <a
-          className="btn btn-maroon btn-medium"
-          href={getRelativePath(href)}
-          >
-          {linkLabel}
+          <a className="btn btn-maroon btn-medium" href={getRelativePath(href)}>
+            {linkLabel}
           </a>
         ) : (
-        <a
-          className="btn btn-maroon btn-medium"
-          target="_blank" rel="noreferrer"
-          href={href}
+          <a
+            className="btn btn-maroon btn-medium"
+            target="_blank"
+            rel="noreferrer"
+            href={href}
           >
-          {linkLabel}
+            {linkLabel}
           </a>
         )}
       </p>
